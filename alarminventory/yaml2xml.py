73fa02e971b2c5a51982cb910a4a3ec846fc912b
jerzyjamroz <jerzy.jamroz@ess.eu>
2020-06-15 15:08:47 +0200

    Devel ongoing.

--- alarminventory/yaml2xml.py
+++ alarminventory/yaml2xml.py
@@ -5,7 +5,11 @@ import os
 import json
 import logging
 import yaml
-import xml
+#import xml
+import dicttoxml
+import xmltodict
+import xmlplain
+import xml.etree.ElementTree as ET
 # local functions
 from genericlibs import *
 # local runtime functions
@@ -13,17 +17,37 @@ from genericlibs import *
 
 def main(in_yl, out_xl):
     logging.info(__file__)
-    # Get the script root location
-    # root = __file__.replace(os.path.basename(__file__), "")
-    # root = "."
+
     with open(in_yl) as infile:
-        input_yf = yaml.load(infile)
+        input_yaml = yaml.load(infile)
 
+    print(input_yaml)
 
-#    with open(out_xl, 'w') as outfile:
-#        xml.dump(tim_net_json, outfile)
+    with open("in/example.xml") as infile:
+        xml_dict = xmltodict.parse(infile.read())
 
-    #logging.info(tim_net_json)
+    print(xml_dict)
+    #output = xmlplain.obj_from_yaml(input_yaml)
+    #output = input_yaml
+    #output = xmlplain. (input_yaml)
+    #print(output)
+    #output_xml = dicttoxml(input_yaml, custom_root='test', attr_type=False)
+    output_xml = dicttoxml.dicttoxml(xml_dict)
+    output_xml_str = ET.tostring(output_xml, encoding='unicode', method='xml')
+    print("-------------------------------------------------------")
+    print(type(output_xml_str))
+    print(output_xml_str)
+    #with open(out_xl, 'w') as outfile:
+    #    xmlplain.xml_from_obj(output, outfile, pretty=True)
+        #xml.dump(output, outfile)
+
+    #with open(out_xl, 'w') as outfile:
+    #    outfile.write(str(output_xml))
+    #    #xml.dump(output, outfile)
+    with open(out_xl, 'w') as outfile:
+        outfile.write(str(output_xml_str))
+
+    logging.info(output_xml)
 
 
 if __name__ == '__main__':
