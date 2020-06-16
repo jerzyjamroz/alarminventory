a44ae9f74ae8926e877ab9026e1e4cb1ad2b2992
jerzyjamroz <jerzy.jamroz@ess.eu>
2020-06-16 14:55:32 +0200

    Inventory devel.

--- alarminventory/yaml2xml.py
+++ alarminventory/yaml2xml.py
@@ -1,55 +1,80 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import json
 import logging
-import yaml
-#import xml
-import dicttoxml
+import oyaml
+# import yamlordereddictloader
+# import dicttoxml
 import xmltodict
-import xmlplain
-import xml.etree.ElementTree as ET
-# local functions
-from genericlibs import *
-# local runtime functions
 
 
 def main(in_yl, out_xl):
     logging.info(__file__)
 
     with open(in_yl) as infile:
-        input_yaml = yaml.load(infile)
+        input_yaml = yaml.load(infile, Loader=yaml.FullLoader)
 
-    print(input_yaml)
+    # print(input_yaml)
 
     with open("in/example.xml") as infile:
         xml_dict = xmltodict.parse(infile.read())
 
-    print(xml_dict)
-    #output = xmlplain.obj_from_yaml(input_yaml)
-    #output = input_yaml
-    #output = xmlplain. (input_yaml)
-    #print(output)
-    #output_xml = dicttoxml(input_yaml, custom_root='test', attr_type=False)
-    output_xml = dicttoxml.dicttoxml(xml_dict)
-    output_xml_str = ET.tostring(output_xml, encoding='unicode', method='xml')
-    print("-------------------------------------------------------")
-    print(type(output_xml_str))
-    print(output_xml_str)
-    #with open(out_xl, 'w') as outfile:
-    #    xmlplain.xml_from_obj(output, outfile, pretty=True)
-        #xml.dump(output, outfile)
+    # print(xml_dict)
+    xml = dicttoxml.dicttoxml(xml_dict)
+    print(xml)
 
-    #with open(out_xl, 'w') as outfile:
-    #    outfile.write(str(output_xml))
-    #    #xml.dump(output, outfile)
     with open(out_xl, 'w') as outfile:
-        outfile.write(str(output_xml_str))
+        outfile.write(xml.decode())
 
-    logging.info(output_xml)
+    # logging.info(output_xml)
+
+
+def demo1_xml2xml():
+    with open("../in/demo.xml") as infile:
+        xml_dict = xmltodict.parse(infile.read())
+
+    print(xml_dict)
+
+    with open("../out/demo.xml", 'w') as outfile:
+        outfile.write(xmltodict.unparse(xml_dict, encoding='utf-8', pretty=True))
+
+
+def demo2_yaml2xml():
+    with open("../in/example.yaml") as infile:
+        input_yaml = yaml.load(infile, Loader=yaml.FullLoader)
+
+    with open("../out/demo.xml", 'w') as outfile:
+        outfile.write(xmltodict.unparse(input_yaml, encoding='utf-8', pretty=True))
+
+
+def demo3_xml2yaml():
+    with open("../in/demo.xml") as infile:
+        input_dict = xmltodict.parse(infile.read())
+
+    #output_dict = json.loads(json.dumps(input_dict))
+    #print(output_dict)
+
+    with open("../out/demo.yaml", 'w') as outfile:
+        yaml.dump(input_dict, outfile, default_flow_style=False, sort_keys=False)
+
+
+def demo4_yaml2xml():
+    with open("../in/demo.yaml") as infile:
+        input_yaml = yaml.load(infile, Loader=yamlordereddictloader.Loader)
+
+    with open("../out/demo.xml", 'w') as outfile:
+        outfile.write(xmltodict.unparse(input_yaml, encoding='utf-8', pretty=True))
+
+
+def demo5_yaml2xml_basic():
+    with open("../in/demo.yaml") as infile:
+        input_yaml = oyaml.load(infile, Loader=oyaml.FullLoader)
+
+    with open("../out/demo.xml", 'w') as outfile:
+        outfile.write(xmltodict.unparse(input_yaml, encoding='utf-8', pretty=True))
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    demo5_yaml2xml_basic()
 
