aa030e01daa5f8aaa508aa0e0f695b5f13186882
jerzyjamroz <jerzy.jamroz@ess.eu>
2020-06-18 11:59:58 +0200

    Lab tests.

--- alarminventory/menu.py
+++ alarminventory/menu.py
@@ -9,7 +9,7 @@ def menu():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbosity", action="count", default=0)
     parser.add_argument("-i", "--inventory", action='store', default="inventory/MAIN-TEST.yaml", help="Read the PV inventory")
-    parser.add_argument("-o", "--output", action='store', default="output/example.xml", help="Output the alarms for the EPICS BEAST alarm system")
+    parser.add_argument("-o", "--output", action='store', default="output/out.xml", help="Output the alarms for the EPICS BEAST alarm system")
     parser.add_argument("-m", "--manual", action='store_true', help="Print the application manual")
     args = parser.parse_args()
     if args.verbosity >= 2:
